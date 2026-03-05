a9ac597ef79e36746fb6de68db8ff9c9b7c3c28f
Lavanya Shukla <112153884+LavanyaShukla3@users.noreply.github.com>
2026-03-05 20:20:42 +0530

    Autowire By Name HandsOn

--- lec26/src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ lec26/src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -1,13 +1,26 @@
 package com.loose.coupling;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class LooseCouplingExample {
     public static void main(){
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationIocLooseCoupling.xml");
+
         UserDataProvider udp = new UserDatabaseProvider();
         UserManager umWithDB = new UserManager(udp);
         System.out.println(umWithDB.getUserInfo());
 
+
         UserDataProvider webServiceProvider = new WebServiceDataProvider();
         UserManager umWithWebService = new UserManager(webServiceProvider);
         System.out.println(umWithWebService.getUserInfo());
+
+        UserManager manager =
+                (UserManager) context.getBean("userManagerWithDB");
+        System.out.println(manager.getUserInfo());
+
+
     }
 }
