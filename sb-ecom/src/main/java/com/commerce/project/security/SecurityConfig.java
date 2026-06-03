ab455523157fb52a8b93490dde6dd7ca1d57785b
Lavanya Shukla <112153884+LavanyaShukla3@users.noreply.github.com>
2026-06-04 01:47:35 +0530

    Understood Spring Security filter chain concept,Configured basic in-memory authentication, implemented form-based vs basic authentication

---
+++ sb-ecom/src/main/java/com/commerce/project/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.commerce.project.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+//            .httpBasic() -"Use the header" so basic auth type of authentication
+//            .sessionManagement(STATELESS)- "Don't remember me at all"
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/public/**").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user1").password("{noop}password1").roles("USER").build();
+        UserDetails user2 = User.withUsername("admin").password("{noop}password2").roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+}
+
+
